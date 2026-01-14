83a6397b829b8f42457a428032eee04268c5e490
Jose Javier Mirador <josejaviersmirador@gmail.com>
2026-01-14 12:22:34 +0800

    Modify Vue app message and add image list
    Updated the Vue app to change the message and added an image list.

--- js/vue.js
+++ js/vue.js
@@ -1,9 +1,14 @@
  const app = Vue.createApp({
-   data() {
-    return {
-     message: "Hello World!"
+    data() {
+        return {
+         message : 'Hello po!',
+            imageList: [
+                { id: 1, src: 'image/okada.jpg', alt: 'Profile 1' },
+                { id: 2, src: 'image/okada.jpg', alt: 'Profile 2' },
+                { id: 3, src: 'image/okada.jpg', alt: 'Profile 3' }
+            ]
+        }
     }
-   }
-  })
-  
-  app.mount('#app')
+})
+
+app.mount('#profile-page-body')
