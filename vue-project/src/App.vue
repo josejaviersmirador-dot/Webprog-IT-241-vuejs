743748777f314217f7f372b77a1b3e6b072dba0d
Jose Javier Mirador <josejaviersmirador@gmail.com>
2026-01-14 06:12:31 +0000

    Updated food list

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,11 +1,20 @@
-<script setup lang="ts"></script>
+<script setup>
+  import { ref } from 'vue'
+
+  const foods = ref([
+  { id: 1, name: 'Pizza', desc: 'I love Pizza' }
+  { id: 2, name: 'Apples', desc: 'I like apples' },
+  { id: 3, name: 'Bread', desc: 'I like bread ' }
+])
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <h1>Food</h1>
+  <div v-for="food in foods" :key="food.id">
+    <h2>{{ food.name }}</h2>
+    <p>{{ food.desc }}</p>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+</style>
